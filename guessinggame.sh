067ee262ba3d82705674b571e0fdd9020b3f46e1
simran <simran.jain@mca.christuniversity.in>
2020-05-28 22:20:41 +0530

    added guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,24 @@
+
+function input
+{
+  echo $(date)
+}
+
+filenum=(*)
+filenum=${#filenum[@]}
+
+while [[ $filenum -ne $n ]]
+do
+    echo "How many files are there in this directory? "
+    read n
+  if [[ $filenum -eq $n ]]
+  then
+    echo "Congratulations! You have guessed it correct!"
+  elif [[ $filenum -gt $n ]]
+  then
+    echo "Your guess is too low. Please try again."
+  else
+    echo "Your guess is too high. Please try again."
+  fi
+done
+
